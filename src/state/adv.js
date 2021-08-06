d109ab67fc1c12c4691f2df14e10ea264531567e
Tucker <tuckerhoog@tutanota.com>
2021-08-06 12:56:07 -0400

    custom hook for listing all adventurers

---
+++ src/state/adv.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { fetchAdv } from '../services/listAPI';
+
+export const useAdv = (paging) => {
+  
+  const [loading, setLoading] = useState(true);
+  const[adventurers, setAdventurers] = useState([]);
+
+  useEffect(() => {
+    fetchAdv(page)
+    .then(setAdventurers)
+    .finally(() => setLoading(false));
+  }, [pagin]);
+
+  return { adventurers, loading };
+};
